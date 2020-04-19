d89ff2eaf37108bc29f1cf6f1552ba944aa61afe
LiuYan <616098210@qq.com>
2020-04-20 00:35:38 +0800

    购物街

---
+++ src/components/content/backtop/BackTop.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="back-top">
+    <img src="@/assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackTop"
+}
+</script>
+
+<style scoped>
+
+</style>
