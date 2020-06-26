da4a579ab9ed4d7b71edfcbf3d54bd4c6fdfea95
LiuYan <616098210@qq.com>
2020-06-26 11:36:10 +0800

    '最新

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FastClick from 'fastclick'
+
 import toast from './components/common/toast'
 
 Vue.config.productionTip = false
@@ -10,7 +12,8 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 // 安装toast插件
 Vue.use(toast)
-
+// 解决移动客户端的300毫秒延迟
+FastClick.attach(document.body)
 
 new Vue({
   router,
