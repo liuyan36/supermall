9518f39c8901f9e6ef26072bc7c0fddded44c8e6
liuyan36 <616098210@qq.com>
2020-09-27 09:53:53 +0800

    第一次提交

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -11,7 +11,7 @@
           <tab-control :titles="['综合', '新品', '销量']"
                        @itemClick="tabClick" class="tab-controls"></tab-control>
           <tab-content-detail :category-detail="showCategoryDetail"></tab-content-detail>
-        </div>
+        </div>1
       </scroll>
     </div>
   </div>
